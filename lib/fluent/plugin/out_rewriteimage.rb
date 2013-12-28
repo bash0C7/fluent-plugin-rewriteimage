8b9049dd71f528adb45aaba2ab421f4bf64b17e7
Toshiaki Koshiba <koshiba@qg8.so-net.ne.jp>
2013-12-29 01:21:00 +0900

    Implement spec

--- lib/fluent/plugin/out_rewriteimage.rb
+++ lib/fluent/plugin/out_rewriteimage.rb
@@ -3,14 +3,13 @@ require 'open-uri'
 
 module Fluent
   class Fluent::RewriteImageOutput < Fluent::Output
+    include Fluent::HandleTagNameMixin
+
     Fluent::Plugin.register_output('rewriteimage', self)
 
     def initialize
       super
     end
-
-    config_param :remove_prefix,   :string, :default => nil
-    config_param :add_prefix,      :string, :default => nil
 
     config_param :image_source_key, :string
     config_param :image_key, :string
@@ -19,12 +18,13 @@ module Fluent
     def configure(conf)
       super
 
-      if @remove_prefix
-        @removed_prefix_string = @remove_prefix + '.'
-        @removed_length = @removed_prefix_string.length
-      end
-      if @add_prefix
-        @added_prefix_string = @add_prefix + '.'
+      if (
+          !remove_tag_prefix &&
+          !remove_tag_suffix &&
+          !add_tag_prefix    &&
+          !add_tag_suffix
+      )
+        raise ConfigError, "out_rewriteimage: At least one of remove_tag_prefix/remove_tag_suffix/add_tag_prefix/add_tag_suffix is required to be set."
       end
     end
 
@@ -39,28 +39,22 @@ module Fluent
     end
 
     def emit(tag, es, chain)
-      if @remove_prefix and
-          ((tag.start_with?(@removed_prefix_string) && tag.length > @removed_length) || tag == @remove_prefix)
-        tag = tag[@removed_length..-1] || ''
-      end
-
-      if @add_prefix
-        tag = tag && tag.length > 0 ? @added_prefix_string + tag : @add_prefix
-      end
-
       es.each {|time,record|
-        raw_media = open(record[@image_source_key]).read
-        
-        record[@image_key] = if @base64encoded
-          Base64.decode64(raw_media) 
-        else
-          raw_media
-        end
-
-        Engine.emit(tag, time, record)
+        t = tag.dup
+        filter_record(t, time, record)
+        Engine.emit(t, time, record)
       }
 
       chain.next
     end
+
+    def filter_record(tag, time, record)
+      raw_media = open(record[@image_source_key], 'rb').read
+        
+      record[@image_key] = @base64encode ? Base64.encode64(raw_media) : raw_media
+
+      super(tag, time, record)
+    end
+
   end
 end
--- spec/lib/fluent/plugin/out_rewriteimage_spec.rb
+++ spec/lib/fluent/plugin/out_rewriteimage_spec.rb
@@ -1,5 +1,52 @@
 require 'spec_helper'
 
 describe do
+  let(:driver) {Fluent::Test::OutputTestDriver.new(Fluent::RewriteImageOutput, 'test.metrics').configure(config)}
+  let(:image_file) {File.join(File.dirname(__FILE__), 'imagefile.jpg')}
+
+  let(:instance) {driver.instance}
+  let(:record) {{'image_field' => image_file}}
+  let(:time) {0}
+  let(:filter) {
+    d = driver
+    r = record.dup
+    d.instance.filter_record('test', Time.now, r)
+    
+    r
+  }
+
+  describe 'emit' do
+
+    context 'base64encoded_false' do
+      let(:config) {
+        %[
+           add_tag_prefix hoge
+           image_source_key image_field
+           image_key rewrited_image_field
+           base64encode false
+        ]
+      }
+
+      it do
+        filter['rewrited_image_field'].should eq File.open(image_file, 'rb').read
+      end
+    end
+
+    context 'base64encoded_true' do
+      let(:config) {
+        %[
+           add_tag_prefix hoge
+           image_source_key image_field
+           image_key rewrited_image_field
+           base64encode true
+        ]
+      }
+
+      it do
+        Base64.decode64(filter['rewrited_image_field']).should eq File.open(image_file, 'rb').read
+      end
+    end
+    
+  end
 
 end
